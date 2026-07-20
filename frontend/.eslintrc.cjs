e5aee739335450bc20f1c4ce78b268be64dbbb2c
Sebastián <sebastiangiannini95@gmail.com>
2026-07-20 11:06:03 -0300

    chore: configuración de ESlint

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2020: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ['@typescript-eslint', 'react-hooks', 'react-refresh'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    ignorePatterns: ['dist', 'build', 'coverage', 'node_modules'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+                caughtErrorsIgnorePattern: '^_',
+            },
+        ],
+    },
+};
